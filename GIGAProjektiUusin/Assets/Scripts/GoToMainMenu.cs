4f89da9781a121cf8811e4f917cf634ad5228d0e
Lurppa <53574763+lurpeeBurbee@users.noreply.github.com>
2022-12-21 13:56:32 +0200

    Added some functions:
    Timeline: train arrives and leaves.
    Button to make the train leave.
    Particle effect.
    Fixed Menu visibility, centered the 3 buttons.

--- GIGAProjektiUusin/Assets/Scripts/GoToMainMenu.cs
+++ GIGAProjektiUusin/Assets/Scripts/GoToMainMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class GoToMainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
         SceneManager.LoadScene("MainMenu");
